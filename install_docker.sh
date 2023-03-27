eab78e347545d13438e3d5c356f1def41822145d
ryohei.kobayashi <ryohei.kobayashi@okadanet.org>
2023-03-27 14:06:16 -0600

    add install_docker

---
+++ install_docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+### update and install pkg ###
+sudo apt update -y
+
+### install Docker ###
+sudo apt update -y 
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update -y
+sudo apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo usermod -aG docker $USER 
+
+#install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+### nvidia-docker install ###
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt update
+sudo apt install -y nvidia-docker2
+sudo systemctl restart docker
+
+#echo 'installer finished. Please reboot your computer.'
